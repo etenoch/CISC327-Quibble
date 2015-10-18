9a8eb522e281aae61d83ea8cfb94ab6132db11ea
Enoch Tam <et.enoch@gmail.com>
2015-10-18 13:44:46 -0400

    + some comments

--- quibble.py
+++ quibble.py
@@ -10,6 +10,7 @@ from lib.Transaction import Transaction
 from lib.DailyTransactions import DailyTransactions
 from lib.User import User
 
+## initialize program ##
 programloop = True
 
 currentEvents = CurrentEvents()
@@ -19,18 +20,22 @@ dailyTransactions = DailyTransactions()
 
 user = None
 
+
+## Main Program Loop ##
 while programloop:
     command = raw_input().strip()
 
-    if user is None:
+    if user is None:  ## User hasn't logged in yet
         if command == "login":
             command = raw_input().strip()
             if command == "sales":
                 user = User(False)
             elif command == "admin":
                 user = User(True)
+            else:
+                print "throw error"  # only sales or admin allowed
         else:
-            print "throwerror"
+            print "throw error"  # only login command allowed
     else:
         if command == "logout":
             programloop=False
@@ -39,28 +44,30 @@ while programloop:
             if user.validCommand(Transaction.SELL):
                 eventName = raw_input()
             else:
-                print "throw error"
+                print "throw error"  # permissions error
         elif command == "return":
             if user.validCommand(Transaction.RETURN):
                 print "do stuff"
             else:
-                print "throw error"
+                print "throw error"  # permissions error
         elif command == "create":
             if user.validCommand(Transaction.CREATE):
                 print "do stuff"
             else:
-                print "throw error"
+                print "throw error"  # permissions error
         elif command == "add":
             if user.validCommand(Transaction.ADD):
                 print "do stuff"
             else:
-                print "throw error"
+                print "throw error"  # permissions error
         elif command == "delete":
             if user.validCommand(Transaction.DELETE):
                 print "do stuff"
             else:
-                print "throw error"
+                print "throw error"  # permissions error
         else:
-            print "throw error"
+            print "throw error"  # invalid command
 
+
+## Write transactions to file ##
 dailyTransactions.toFile("filepath")  # write output file
