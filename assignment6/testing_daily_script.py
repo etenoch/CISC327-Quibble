9ebbef960dfb29150f678256ee769f30a403594c
Enoch Tam <et.enoch@gmail.com>
2015-12-04 13:39:48 -0500

    make some test structure changes

--- assignment6/testing_daily_script.py
+++ assignment6/testing_daily_script.py
@@ -6,9 +6,10 @@
 
 import sys
 import os
+import os.path
 
 front_end_code = "code_front_final/quibble.py"
-back_end_code = "code_backend_final/quibble_backend.py"
+back_end_code = "code_back_final/quibble_backend.py"
 
 # defualt
 base_path = "day_1/"
@@ -22,12 +23,14 @@ merged_transactions_file = base_path+"merged_transactions_file.txt"
 new_master_events_file = base_path+"outputs/master_events_file.txt"
 new_current_events_file = base_path+"outputs/current_events_file.txt"
 
+transactions = ["transaction_1.txt","transaction_2.txt","transaction_3.txt","transaction_4.txt"]
+
 if len(sys.argv)>1: # called from weekly script
 	cmd_arguments = sys.argv
 	day = cmd_arguments[1]
 	inputs = cmd_arguments[2]
 	outputs = cmd_arguments[3]
-	cases = cmd_arguments[4:]
+	transactions = cmd_arguments[4:]
 
 	base_path = day+"/"
 	old_master_events_file = inputs+"/master_events_file.txt"
@@ -39,11 +42,13 @@ if len(sys.argv)>1: # called from weekly script
 	new_master_events_file = outputs+"/master_events_file.txt"
 	new_current_events_file = outputs+"/current_events_file.txt"
 
+
+open(merged_transactions_file,"w").close()
 # run frontend cases
-for case in ["case1","case2","case3","case4"]:
+for case in transactions:
 
 	# run frontend code
-	os.system(front_end_code+" "+old_current_events_file+" "+temp_transactions_file)
+	os.system("python "+front_end_code+" "+old_current_events_file+" "+temp_transactions_file +" < "+base_path+case)
 
 	lines = open(temp_transactions_file).readlines()
 	if lines[-1][:2]=="00": # remove last line if 00
@@ -51,10 +56,10 @@ for case in ["case1","case2","case3","case4"]:
 
 	mergedFile = open(merged_transactions_file,"a")
 	for item in lines:
-			mergedFile.write("%s\n" % item)
+			mergedFile.write("%s" % item)
+	mergedFile.close()
 
-
-	# run backend
-os.system(back_end_code+" "+old_master_events_file+" "+new_master_events_file+" "+new_current_events_file+" "+merged_transactions_file)
+# run backend
+os.system("python "+back_end_code+" "+old_master_events_file+" "+merged_transactions_file+" "+new_master_events_file+" "+new_current_events_file)
 
 
--- assignment6/testing_weekly_script.py
+++ assignment6/testing_weekly_script.py
@@ -10,37 +10,37 @@ daily_testing_script = "testing_daily_script.py"
 days = [
 		{
 			'day':"day_1", 
-			'cases':["case1","case2","case3","case4"]
+			'transactions':["transaction_1.txt","transaction_2.txt","transaction_3.txt","transaction_4.txt"]
 			'inputs':"day_1/inputs",
 			'outputs':"day_1/outputs"
 		},
 		{
 			'day':"day_2",	
-			'cases':["case1","case2"]
+			'transactions':["transaction_1.txt","transaction_2.txt"]
 			'inputs':"day_1/outputs",
 			'outputs':"day_2/outputs"
 		},
 		{
 			'day':"day_3",	
-			'cases':["case1","case2"]
+			'transactions':["transaction_1.txt","transaction_2.txt"]
 			'inputs':"day_2/outputs",
 			'outputs':"day_3/outputs"
 		},
 		{
 			'day':"day_3",	
-			'cases':["case1","case2"]
+			'transactions':["transaction_1.txt","transaction_2.txt"]
 			'inputs':"day_2/outputs",
 			'outputs':"day_3/outputs"
 		},
 		{
 			'day':"day_4",	
-			'cases':["case1","case2"]
+			'transactions':["transaction_1.txt","transaction_2.txt"]
 			'inputs':"day_3/outputs",
 			'outputs':"day_4/outputs"
 		},
 		{
 			'day':"day_5",	
-			'cases':["case1","case2"]
+			'transactions':["transaction_1.txt","transaction_2.txt"]
 			'inputs':"day_4/outputs",
 			'outputs':"day_5/outputs"
 		}
@@ -48,4 +48,4 @@ days = [
 
 
 for d in days:
-	os.system(daily_testing_script+" "+d['day']+" "+d['inputs']+" "+d['ouptuts']+" "+" ".join(d['cases']))
+	os.system(daily_testing_script+" "+d['day']+" "+d['inputs']+" "+d['ouptuts']+" "+" ".join(d['transactions']))
